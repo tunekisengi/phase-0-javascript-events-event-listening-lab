d0a9ec3def43a4541728a2ec253c5884183bff79
jamesmutune <tunekivasu01@gmail.com>
2024-01-12 23:34:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    let input = document.getElementById('input')
+    input.addEventListener('click', function() {
+        alert("I was clicked!")
+
+
+    })
 }
+addingEventListener();
